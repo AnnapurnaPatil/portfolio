bef118573aa456d4ab2b37c66554fca6719dbf12
annapurnapatil <annapurnapatil23@gmail.com>
2025-08-08 21:32:29 +0200

    add new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,11 +95,11 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
   icons: {
     icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/annapurnapatil.png?v=1", sizes: "32x32", type: "image/png" },
+      { url: "/annapurnapatil.png?v=1", sizes: "16x16", type: "image/png" },
     ],
     apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+      { url: "/annapurnapatil.png?v=1", sizes: "180x180", type: "image/png" },
     ],
     other: [
       {
